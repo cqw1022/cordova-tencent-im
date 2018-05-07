56bb47c971a12e93e49f15781fe425cbae0c7574
caiqiwen <cqw1022@gmail.com>
2018-05-07 16:50:33 +0800

    腾讯im ios版本

---
+++ src/ios/CDVTxim.h
@@ -0,0 +1,45 @@
+//
+//  CDVWxpay.h
+//  cordova-plugin-wxpay
+//
+//  Created by tong.wu on 06/30/15.
+//
+//
+
+#import <Cordova/CDV.h>
+#import "WXApi.h"
+#import "WXApiObject.h"
+
+@interface CDVTxim:CDVPlugin <TIMConnListener>
+
+@property (nonatomic, strong) NSString *currentCallbackId;
+@property (nonatomic, strong) TIMConversation *conversation;
+@property (nonatomic, strong) IMALoginParam *_loginParam;
+// @property (nonatomic, strong) IMAConversationManager *_conversationMgr;
+
+- (void)initSdk:(CDVInvokedUrlCommand *)command;
+- (void)login:(CDVInvokedUrlCommand *)command;
+- (void)logout:(CDVInvokedUrlCommand *)command;
+- (void)requestAddFriend:(CDVInvokedUrlCommand *)command;
+
+// 初始化
+// 登录
+// 退出登录
+// 消息：onNewMessage，包括添加好友请求，其他通知等
+
+// 添加好友请求,客户端直接调用
+
+// 同意添加好友，先向 游戏服务器请求（等第一个版本处理完再处理），然后再调用同意接口
+// 私聊
+// 黑名单，设置黑名单
+// 获取好友，获取黑名单
+// 删除好友
+
+
+// - (void)getConversation:(CDVInvokedUrlCommand *)command;
+// 离线消息
+// 未读消息：登录后获取所有回话，然后获取漫游信息
+// 获取本地存储对话，私聊存储
+// 发送消息
+// 新的消息
+@end
---
+++ src/ios/CDVTxim.m
@@ -0,0 +1,250 @@
+//
+//  CDVWxpay.m
+//  cordova-plugin-wxpay
+//
+//  Created by tong.wu on 06/30/15.
+//
+//
+
+#import "CDVWxpay.h"
+
+@implementation CDVWxpay
+
+#pragma mark "API"
+
+- (void)initSdk:(CDVInvokedUrlCommand *)command
+{
+    [self.commandDelegate runInBackground:^{
+        // check arguments
+        NSDictionary *params = [command.arguments objectAtIndex:0];
+        if (!params)
+        {
+            [self failWithCallbackID:command.callbackId withMessage:@"参数格式错误"];
+            return ;
+        }
+        
+        NSString *sdkAppId = nil;
+        NSString *accountType = nil;
+
+        // check the params
+        if (![params objectForKey:@"sdkAppId"])
+        {
+            [self failWithCallbackID:command.callbackId withMessage:@"sdkAppId参数错误"];
+            return ;
+        }
+        sdkAppId = [params objectForKey:@"sdkAppId"];
+
+        if (![params objectForKey:@"accountType"])
+        {
+            [self failWithCallbackID:command.callbackId withMessage:@"accountType参数错误"];
+            return ;
+        }
+        accountType = [params objectForKey:@"accountType"];
+
+
+
+
+        TIMManager *manager = [TIMManager sharedInstance];
+        TIMSdkConfig *config = [[TIMSdkConfig alloc] init];
+        config.sdkAppId = sdkAppId ;
+        config.accountType = accountType;
+        config.disableCrashReport = NO;
+
+        BOOL isAutoLogin = [IMAPlatform isAutoLogin];
+        if (isAutoLogin)
+        {
+            _loginParam = [IMALoginParam loadFromLocal];
+        }
+        else
+        {
+            _loginParam = [[IMALoginParam alloc] init];
+        }
+        
+        [IMAPlatform configWith:_loginParam.config];
+
+        CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"调起成功"];
+        
+        [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];
+    }];
+}
+
+// - (IMAConversationManager *)conversationMgr
+// {
+//     if (!_conversationMgr)
+//     {
+//         _conversationMgr = [[IMAConversationManager alloc] init];
+//     }
+//     return _conversationMgr;
+// }
+
+- (void)login:(CDVInvokedUrlCommand *)command
+{
+    self.currentCallbackId = command.callbackId;
+
+
+    NSString *identifier = nil;
+    NSString *userSig = nil;
+    NSString *appidAt3rd = nil;
+
+    // check the params
+    if (![params objectForKey:@"identifier"])
+    {
+        [self failWithCallbackID:command.callbackId withMessage:@"identifier 参数错误"];
+        return ;
+    }
+    identifier = [params objectForKey:@"identifier"];
+
+    if (![params objectForKey:@"userSig"])
+    {
+        [self failWithCallbackID:command.callbackId withMessage:@"userSig 参数错误"];
+        return ;
+    }
+    userSig = [params objectForKey:@"userSig"];
+
+    if (![params objectForKey:@"appidAt3rd"])
+    {
+        [self failWithCallbackID:command.callbackId withMessage:@"appidAt3rd 参数错误"];
+        return ;
+    }
+    userSig = [params objectForKey:@"appidAt3rd"];
+
+
+    _loginParam.identifier = identifier;
+    _loginParam.userSig = userSig;
+    _loginParam.tokenTime = [[NSDate date] timeIntervalSince1970];
+    _loginParam.appidAt3rd = appidAt3rd;
+    
+
+    [[IMAPlatform sharedInstance] login:_loginParam succ:^{
+        
+        DebugLog(@"登录成功:%@ tinyid:%llu sig:%@", param.identifier, [[IMSdkInt sharedInstance] getTinyId], param.userSig);
+        // [IMAPlatform setAutoLogin:YES];
+        //去掉此处的获取群里表，放到IMAPlatform+IMSDKCallBack 的 onRefresh中去，如果直接在这里获取群里表，第一次安装app时，会拉去不到群列表
+//        [ws configGroup];
+        
+        [self successWithCallbackID:self.currentCallbackId];
+    } fail:^(int code, NSString *msg) {
+        [self failWithCallbackIDa:self.currentCallbackId];
+    }];
+}
+
+- (void)logout:(CDVInvokedUrlCommand *)command
+{
+    self.currentCallbackId = command.callbackId;
+    [[TIMManager sharedInstance] logout:^(){
+        [self successWithCallbackID:self.currentCallbackId];
+    } fail:^(int code, NSString *msg) {
+        [self failWithCallbackIDa:self.currentCallbackId];
+    }];
+}
+
+
+// - (void)requestAddFriend:(CDVInvokedUrlCommand *)command
+// {
+        
+//     self.currentCallbackId = command.callbackId;
+//     NSMutableArray * users = [[NSMutableArray alloc] init];
+
+
+//     NSString *identifier = nil;
+//     NSString *remark = nil;
+//     NSString *addWording = nil;
+
+//     // check the params
+//     if (![params objectForKey:@"identifier"])
+//     {
+//         [self failWithCallbackID:command.callbackId withMessage:@"identifier 参数错误"];
+//         return ;
+//     }
+//     identifier = [params objectForKey:@"identifier"];
+
+//     if (![params objectForKey:@"remark"])
+//     {
+//         [self failWithCallbackID:command.callbackId withMessage:@"remark 参数错误"];
+//         return ;
+//     }
+//     remark = [params objectForKey:@"remark"];
+
+//     if (![params objectForKey:@"addWording"])
+//     {
+//         [self failWithCallbackID:command.callbackId withMessage:@"addWording 参数错误"];
+//         return ;
+//     }
+//     addWording = [params objectForKey:@"addWording"];
+
+
+
+//     TIMAddFriendRequest* req = [[TIMAddFriendRequestalloc] init];
+//     // 添加好友 iOS_002
+//     req.identifier = identifier;
+//     // 添加备注 002Remark
+//     req.remark = remark;
+//     // 添加理由
+//     req.addWording = addWording;
+//     [users addObject:req];
+//     [[TIMFriendshipManager sharedInstance] addFriend:users succ:^(NSArray * arr) {
+//         for (TIMFriendResult * res in arr) {
+//             if (res.status != TIM_FRIEND_STATUS_SUCC) {
+//                 [self successWithCallbackID:command.callbackId];
+//             }
+//             else {
+//                 NSLog(@"AddFriend succ: user=%@ status=%d", res.identifier, res.status);
+//                 [self failWithCallbackID:command.callbackId withMessage: res.status];
+//             }
+//         }
+//     } fail:^(int code, NSString * err) {
+//         NSLog(@"add friend fail: code=%d err=%@", code, err);
+//         [self failWithCallbackID:command.callbackId withError: err];
+//     }];
+
+// }
+
+// - (void)getConversation:(CDVInvokedUrlCommand *)command
+// {
+//     self.currentCallbackId = command.callbackId;
+//     TIMConversationType conversationType = nil;
+//     NSString *receiver = nil;
+
+//     // check the params
+//     if (![params objectForKey:@"conversationType"])
+//     {
+//         [self failWithCallbackID:command.callbackId withMessage:@"conversationType参数错误"];
+//         return ;
+//     }
+//     conversationType = [params objectForKey:@"conversationType"];
+
+//     if (![params objectForKey:@"receiver"])
+//     {
+//         [self failWithCallbackID:command.callbackId withMessage:@"receiver参数错误"];
+//         return ;
+//     }
+//     receiver = [params objectForKey:@"receiver"];
+//     self.conversation = [[TIMManager sharedInstance] getConversation:conversationType receiver:receiver];
+
+// }
+
+
+
+- (void)successWithCallbackID:(NSString *)callbackID
+{
+    [self successWithCallbackID:callbackID withMessage:@"OK"];
+}
+
+- (void)successWithCallbackID:(NSString *)callbackID withMessage:(NSString *)message
+{
+    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
+}
+
+- (void)failWithCallbackID:(NSString *)callbackID withError:(NSError *)error
+{
+    [self failWithCallbackID:callbackID withMessage:[error localizedDescription]];
+}
+
+- (void)failWithCallbackID:(NSString *)callbackID withMessage:(NSString *)message
+{
+    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:message];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
+}
+
+@end
